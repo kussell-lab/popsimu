66cae98085f963f5d32ce77d0cca7d3785e18eb1
Mingzhi Lin <mingzhi9@gmail.com>
2015-05-31 22:10:12 -0400

    add threads

--- cmd/popsimu_single/single.go
+++ cmd/popsimu_single/single.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"github.com/mingzhi/fftw"
 	"github.com/mingzhi/gomath/random"
 	"github.com/mingzhi/gomath/stat/correlation"
 	. "github.com/mingzhi/popsimu/cmd"
@@ -120,6 +121,8 @@ type calcConfig struct {
 func calc(simResChan chan popConfig, seqLen int) chan calcConfig {
 	numWorker := runtime.GOMAXPROCS(0)
 	circular := true
+	fftw.InitThreads()
+	fftw.PlanWithNThreads(runtime.NumCPU())
 	dft := correlation.NewFFTW(seqLen, circular)
 	done := make(chan bool)
 
@@ -151,6 +154,7 @@ func calc(simResChan chan popConfig, seqLen int) chan calcConfig {
 
 	go func() {
 		defer dft.Close()
+		defer fftw.CleanupThreads()
 		defer close(calcChan)
 		wait(done, numWorker)
 	}()
