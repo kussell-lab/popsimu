57a12803e13d7e42537100ae88f8e58ddb3fdcc6
Mingzhi Lin <mingzhi9@gmail.com>
2017-02-22 22:27:13 -0500

    collect results

--- cmd/popsimu/two_pops.go
+++ cmd/popsimu/two_pops.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"math/rand"
 	"os"
 	"path/filepath"
 	"runtime"
 	"time"
+
+	"fmt"
 
 	"github.com/mingzhi/popsimu/pop"
 	"github.com/mingzhi/popsimu/simu"
@@ -21,14 +22,15 @@ type Results struct {
 	CalcResults []CalcRes
 }
 
+// CalcRes stores calculation results.
 type CalcRes struct {
-	Index []int
-	Ks    float64
-	Vd    float64
-	Cs    []float64
-	Ct    []float64
-	Cm    []float64
-	Cr    []float64
+	ID string
+	Ks float64
+	Vd float64
+	Cs []float64
+	Ct []float64
+	Cm []float64
+	Cr []float64
 }
 
 func (c *cmdTwoPops) Run(args []string) {
@@ -50,13 +52,13 @@ func (c *cmdTwoPops) Run(args []string) {
 				ks, vd := pop.CalcKs(c.sampleSize, randomSrc, p1)
 				cm, ct, cr, cs := pop.CalcCov(c.sampleSize, c.maxl, randomSrc, p1)
 				res := CalcRes{
-					Index: []int{i},
-					Ks:    ks,
-					Vd:    vd,
-					Cm:    cm,
-					Ct:    ct,
-					Cr:    cr,
-					Cs:    cs,
+					ID: fmt.Sprintf("%d", i),
+					Ks: ks,
+					Vd: vd,
+					Cm: cm,
+					Ct: ct,
+					Cr: cr,
+					Cs: cs,
 				}
 				results.CalcResults = append(results.CalcResults, res)
 				for j := i + 1; j < len(c.popConfigs); j++ {
@@ -64,13 +66,13 @@ func (c *cmdTwoPops) Run(args []string) {
 					ks, vd := pop.CrossKs(c.sampleSize, randomSrc, p1, p2)
 					cm, ct, cr, cs := pop.CrossCov(c.sampleSize, c.maxl, randomSrc, p1, p2)
 					res := CalcRes{
-						Index: []int{i, j},
-						Ks:    ks,
-						Vd:    vd,
-						Cm:    cm,
-						Ct:    ct,
-						Cr:    cr,
-						Cs:    cs,
+						ID: fmt.Sprintf("%d_%d", i, j),
+						Ks: ks,
+						Vd: vd,
+						Cm: cm,
+						Ct: ct,
+						Cr: cr,
+						Cs: cs,
 					}
 					results.CalcResults = append(results.CalcResults, res)
 				}
@@ -87,18 +89,21 @@ func (c *cmdTwoPops) Run(args []string) {
 		}
 	}()
 
-	outFileName := c.prefix + "_res.json"
+	resMap := collect(resChan)
+
+	outFileName := c.prefix + "_res.csv"
 	outFilePath := filepath.Join(c.outdir, outFileName)
-	o, err := os.Create(outFilePath)
+	w, err := os.Create(outFilePath)
 	if err != nil {
 		panic(err)
 	}
-	defer o.Close()
+	defer w.Close()
 
-	for results := range resChan {
-		encoder := json.NewEncoder(o)
-		if err := encoder.Encode(results); err != nil {
-			panic(err)
+	w.WriteString("l,m,v,n,t,b\n")
+	for key, values := range resMap {
+		results := values.Results()
+		for _, res := range results {
+			w.WriteString(fmt.Sprintf("%d,%g,%g,%d,%s,%s\n", res.Lag, res.Mean, res.Variance, res.N, res.Type, key))
 		}
 	}
 }
@@ -120,4 +125,27 @@ func newPop(c pop.Config, src rand.Source) *pop.Pop {
 	g := pop.NewRandomPopGenerator(r, c.Size, c.Length, []byte(c.Alphabet))
 	g.Operate(p)
 	return p
+}
+
+func collect(resChan chan Results) map[string]*Collector {
+	resMap := make(map[string]*Collector)
+	for resCfg := range resChan {
+		for _, res := range resCfg.CalcResults {
+			key := res.ID
+			if _, found := resMap[key]; !found {
+				resMap[key] = NewCollector()
+			}
+
+			var corrResults CorrResults
+			for i := range res.Ct {
+				var corrRes CorrResult
+				corrRes.Lag = i
+				corrRes.Mean = res.Ct[i]
+				corrRes.Type = "P2"
+				corrResults.Results = append(corrResults.Results, corrRes)
+			}
+			resMap[key].Add(corrResults)
+		}
+	}
+	return resMap
 }
